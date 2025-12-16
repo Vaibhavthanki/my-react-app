ce8907497dfca0597a3b48945da5f6f05937d774
Vaibhav Thanki <thankiv27@gmail.com>
2025-12-16 08:20:46 +0530

    refactor: Comment out unused hooks and event handler in Navigation component for clarity

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,7 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+// out of component hooks cannot be used
+// const [isActive, setIsActive] = useState(false);
+// console.log(isActive, "checkboth", setIsActive);
 export const Navigation = () => {
+  // 1 :- Loops
+  // for (let i = 0; i < 5; i++) {
+  //   const [isActive, setIsActive] = useState(false);
+  // }
+
+  // 2 :- Conditional Statements
+  // if(true){
+  //   const [isActive, setIsActive] = useState(false);
+  // }else{
+  //   const [isActive, setIsActive] = useState(true);
+  // }
+
+  // 3 :- Can not call inside other hooks
+  // useEffect(() => {
+  //   const [isActive, setIsActive] = useState(false);
+  //   console.log(isActive, "checkboth", setIsActive);
+  // }, []);
+  // useMemo(() => {
+  //   const [isActive, setIsActive] = useState(false);
+  //   console.log(isActive, "checkboth", setIsActive);
+  // },[])
+  // useCallback(() => {
+  //   const [isActive, setIsActive] = useState(false);
+  //   console.log(isActive, "checkboth", setIsActive);
+  // },[])
+
+  // 4:- Can not call inside event handlers
+  // const handleClick = () => {
+  //   const [isActive, setIsActive] = useState(false);
+  // };
+
+  // if(true){
+  //   return const [isActive, setIsActive] = useState(false);
+  // }else{
+  //   return const [isActive, setIsActive] = useState(true);
+  // }
   return (
     <div className="d-flex justify-content-between p-3 bg-light">
       <div className="row">
@@ -16,6 +55,9 @@ export const Navigation = () => {
             <div className="col mr-3">
               <Link to="/parent">Parent</Link>
             </div>
+            {/* <div className="col mr-3">
+              <button onClick={handleClick}>Registration</button>
+            </div> */}
           </div>
         </div>
       </div>
